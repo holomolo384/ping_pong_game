99783bb09c886c801ad00d0c30608de885d0ace5
holomolo384 <113759571+holomolo384@users.noreply.github.com>
2022-09-24 10:30:09 +0300

    Update ping_pong.py

--- ping_pong.py
+++ ping_pong.py
@@ -1,6 +1,30 @@
 from pygame import *
 
+class GameSprite(sprite.Sprite):
+def __init__(self, player_image, player_x, player_y, player_speed, width, height):
+	super().__init__()
+	self. image = transform.scale(image.load(player_image,(width, heigh)
+	self.speed = player_speed
+	self.rect = self.image.get_rect()										 
+	self.rect.x = player_x
+	self.rect.y = player_y
+def reset(self):
+	def update_r(self):
+		keys = key.get_pressed()
+		if keys[K_UP] and self.rect.y > 5:
+			self.rect.y -= self.speed								 
+		if keys[K_DOWN] and self.rect.y < 420:									 
+			self.rect.y += self.speed											 
+	def update_l(self):
+		keys = key.get_pressed()
+		if keys{
+											
+                                                 
+				
+									 
+
 #картинки 
+											 
 ball_img = "ball.png"
 left_racket = "left_racket.png"
 right_racket = "right_racket.png"
